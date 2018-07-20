4dbd5b0f3f3bf4075b600af539f59348ebc71eba
Thanos <thanos@afrogleap.com>
2018-07-20 17:15:19 +0200

    added getAllRecipes query and connected it to react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { Query } from 'react-apollo';
+import { GET_ALL_RECIPES } from './queries';
 
 class App extends Component {
   render() {
     return (
       <MainAppContainer>
-        home
+        <h1>
+          Home
+        </h1>
+        <Query query={GET_ALL_RECIPES}>
+          {({ data, loading, error }) => {
+            if (loading) {
+              return (
+                <div>
+                  ...Loading...
+                </div>
+              );
+            }
+            if (error) {
+              return (
+                <div>
+                  ...Error...
+                </div>
+              );
+            }
+            console.log(data);
+            return (
+              <div>
+                Recipes
+              </div>
+            );
+          }}
+        </Query>
       </MainAppContainer>
     );
   }
--- src/apollo/apolloClient.js
+++ src/apollo/apolloClient.js
@@ -1,7 +1,8 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: `${process.env.GRPAPHQLURI}`,
+  uri: 'http://localhost:4040/graphql',
+  credentials: 'same-origin',
 });
 
 
