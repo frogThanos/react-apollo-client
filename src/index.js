244078e4a9ac086661054d8be131fde8b1ba199f
Thanos <thanos@afrogleap.com>
2018-07-20 16:47:13 +0200

    connected apollo client to react app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import client from './apollo/apolloClient';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, document.getElementById('root'),
+);
 registerServiceWorker();
