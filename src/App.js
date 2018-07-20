36adf3e3a4c99f2b26b2f277368c5cc593d645b7
Thanos <thanos@afrogleap.com>
2018-07-20 16:37:50 +0200

    added apollo packages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <MainAppContainer>
         home
-      </div>
+      </MainAppContainer>
     );
   }
 }
+
+const MainAppContainer = styled.div`
+  text-align: center;
+`;
 
 export default App;
