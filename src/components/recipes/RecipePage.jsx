1f32907bf84daee9917da40a7b1f3ddfca33da74
Thanos <thanos@afrogleap.com>
2018-07-22 14:47:00 +0200

    created getRecipe Query

--- src/components/recipes/RecipePage.jsx
+++ src/components/recipes/RecipePage.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { Query } from 'react-apollo';
+import { GET_RECIPE } from '../../queries';
 
-const RecipePage = ({ match }) => (
-  <div>
-    {console.log(match.params._id)}
-    RecipePage
-  </div>
+const RecipePage = ({ match: { params: { _id } } }) => (
+  <Query query={GET_RECIPE} variables={{ _id }}>
+    {({ data, loading, error }) => {
+      if (loading) return <div>...loading...</div>;
+      if (error) return <div>...error...</div>;
+      console.log(data);
+      return (
+        <div>
+          Recipe page
+        </div>
+      );
+    }}
+  </Query>
 );
 
 export default withRouter(RecipePage);
