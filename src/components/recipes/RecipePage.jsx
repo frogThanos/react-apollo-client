2a155c8aff69495f0294a486f48f05d712d6e067
Thanos <thanos@afrogleap.com>
2018-07-22 15:13:38 +0200

    Recipe page

--- src/components/recipes/RecipePage.jsx
+++ src/components/recipes/RecipePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { withRouter } from 'react-router-dom';
 import { Query } from 'react-apollo';
 import { GET_RECIPE } from '../../queries';
@@ -9,13 +10,34 @@ const RecipePage = ({ match: { params: { _id } } }) => (
       if (loading) return <div>...loading...</div>;
       if (error) return <div>...error...</div>;
       console.log(data);
+      const {
+        name,
+        category,
+        description,
+        instructions,
+        likes,
+        username,
+      } = data.getRecipe;
       return (
-        <div>
-          Recipe page
-        </div>
+        <RecipePageContainer>
+          <h2>{name}</h2>
+          <p>Category: {category}</p>
+          <p>Description: {description}</p>
+          <p>Instructions: {instructions}</p>
+          <p>Likes: {likes}</p>
+          <p>Created by: {username}</p>
+          <button type="button">
+            like
+          </button>
+        </RecipePageContainer>
       );
     }}
   </Query>
 );
+
+const RecipePageContainer = styled.div`
+  text-align: center;
+  margin-top: 20px;
+`;
 
 export default withRouter(RecipePage);
