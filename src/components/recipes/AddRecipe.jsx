f3f1f9e92a84e2e3efb43af42f2fd8c08f3eb435
Thanos <thanos@afrogleap.com>
2018-07-22 15:39:48 +0200

    addRecipe structure

--- src/components/recipes/AddRecipe.jsx
+++ src/components/recipes/AddRecipe.jsx
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import {
   MainAppContainer,
   FromTitle,
   Form,
   FormInput,
+  FormSelect,
+  FromTextArea,
 } from '../../styled';
 
-const AddRecipe = () => (
-  <MainAppContainer>
-    <FromTitle>AddRecipe</FromTitle>
-  </MainAppContainer>
-);
+class AddRecipe extends PureComponent {
+  handleChange = () => {
+
+  };
+
+  render() {
+    return (
+      <MainAppContainer>
+        <FromTitle>AddRecipe</FromTitle>
+        <Form>
+          <FormInput
+            type="text"
+            name="name"
+            placeholder="Recipe name"
+            onChange={this.handleChange}
+          />
+          <FormSelect name="Category">
+            <option value="Breakfast">Breakfast</option>
+            <option value="Lunch">Lunch</option>
+            <option value="Dinner">Dinner</option>
+            <option value="Snack">Snack</option>
+          </FormSelect>
+          <FormInput
+            type="text"
+            name="description"
+            placeholder="description"
+            onChange={this.handleChange}
+          />
+          <FromTextArea
+            name="instructions"
+            placeholder="instructions"
+            onChange={this.handleChange}
+          />
+          <button type="submit">
+            create recipe
+          </button>
+        </Form>
+      </MainAppContainer>
+    );
+  }
+}
 
 export default AddRecipe;
--- src/styled.js
+++ src/styled.js
@@ -12,9 +12,22 @@ export const FromTitle = styled.h2`
 export const Form = styled.form`
   width: 30%;
   margin: 0 auto;
+  text-align: left;
 `;
 
 export const FormInput = styled.input`
+  display: block;
+  margin-bottom: 20px;
+  width: 100%;
+`;
+
+export const FormSelect = styled.select`
+  display: block;
+  margin-bottom: 20px;
+  width: 100%;
+`;
+
+export const FromTextArea = styled.textarea`
   display: block;
   margin-bottom: 20px;
   width: 100%;
