2c73bb7ad208b74f0dc711337ec95e524c1367a5
Thanos <thanos@afrogleap.com>
2018-07-24 15:07:32 +0200

    parsed the date

--- src/components/profile/UserInfo.jsx
+++ src/components/profile/UserInfo.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const formatDate = (date) => {
+  return new Date(date).toLocaleDateString('nl-NL');
+};
+
 const UserInfo = ({ session }) => (
   <div>
     <h3>User Info</h3>
     <p>Username: {session.getCurrentUser.username}</p>
     <p>Email: {session.getCurrentUser.email}</p>
-    <p>JoinDate: {session.getCurrentUser.joinDate}</p>
+    <p>JoinDate: {formatDate(session.getCurrentUser.joinDate)}</p>
     <ul>
       <h3>{session.getCurrentUser.username}'s Favorites</h3>
       {session.getCurrentUser.favorites.map((favorite) => (
