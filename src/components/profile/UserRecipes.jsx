a8676012fa754f2bb89645baa1f6359f04eb3d77
Thanos <thanos@afrogleap.com>
2018-07-24 16:35:15 +0200

    added delete recipe mutation

--- src/components/profile/UserRecipes.jsx
+++ src/components/profile/UserRecipes.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Query } from 'react-apollo';
-import { GET_USER_RECIPES } from '../../queries';
+import { Query, Mutation } from 'react-apollo';
+import { GET_USER_RECIPES, DELETE_USER_RECIPE } from '../../queries';
+
+const handleDelete = (deleteUserRecipe) => {
+  const confirmDelete = window.confirm('Are you sure you want to delete this recipe?');
+  if (confirmDelete) {
+    deleteUserRecipe().then(({ data }) => {
+      console.log(data);
+    });
+  }
+};
 
 const UserRecipes = ({ username }) => (
   <Query query={GET_USER_RECIPES} variables={{ username }}>
@@ -18,6 +27,35 @@ const UserRecipes = ({ username }) => (
                 <Link to={`/recipes/${recipe._id}`}>
                   <p>{recipe.name}</p>
                 </Link>
+                <Mutation
+                  mutation={DELETE_USER_RECIPE}
+                  variables={{ _id: recipe._id }}
+                  update={(cache, { data: { deleteUserRecipe }}) => {
+                    const { getUserRecipes } = cache.readQuery({
+                      query: GET_USER_RECIPES,
+                      variables: { username },
+                    });
+                    cache.writeQuery({
+                      query: GET_USER_RECIPES,
+                      variables: { username },
+                      data: {
+                        getUserRecipes: getUserRecipes
+                          .filter(recipe => recipe._id !== deleteUserRecipe._id),
+                      },
+                    });
+                  }}
+                >
+                  {(deleteUserRecipe, attrs = {}) => {
+                    return (
+                      <button
+                        type="button"
+                        onClick={() => handleDelete(deleteUserRecipe)}
+                      >
+                        {attrs.loading ? '...deleting...' : 'X'}
+                      </button>
+                    );
+                  }}
+                </Mutation>
               </li>
             ))}
           </ul>
