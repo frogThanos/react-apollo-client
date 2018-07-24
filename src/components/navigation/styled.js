411a53842cd71179b93c17bb01161a159526a5b9
Thanos <thanos@afrogleap.com>
2018-07-24 10:35:54 +0200

    some style changes

--- src/components/navigation/styled.js
+++ src/components/navigation/styled.js
@@ -3,7 +3,11 @@ import styled from 'styled-components';
 export const MainNav = styled.nav`
   background-color: #e74c3c;
   height: 60px;
+  width: 100%;
+  top: 0;
+  margin: 0;
   text-align: center;
+  position: fixed;
 `;
 
 export const Links = styled.ul`
--- src/styled.js
+++ src/styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const MainAppContainer = styled.div`
   text-align: center;
-  margin-top: 20px;
+  margin-top: 80px;
 `;
 
 export const FromTitle = styled.h2`
